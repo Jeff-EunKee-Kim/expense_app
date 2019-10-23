263fe6c6906937466431d3641ee24b1251d29e8b
Jeff Kim <eunkeekim@gmail.com>
2019-10-22 23:00:39 -0400

    progress on making expense app

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import './transaction.dart';
 
 void main() => runApp(MyApp());
 
@@ -16,14 +18,88 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+      id: "t1",
+      title: "New Shoes",
+      amount: 40.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: "t2",
+      title: "New Car",
+      amount: 20.99,
+      date: DateTime.now(),
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Flutter App'),
       ),
-      body: Center(
-        child: Text('Widget Playground!'),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Card(
+            color: Colors.blue,
+            child: Container(
+              width: double.infinity,
+              child: Text('Chart!'),
+            ),
+            elevation: 150,
+          ),
+          Column(
+            children: transactions.map((trans) {
+              return Card(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: <Widget>[
+                    Container(
+                      margin: EdgeInsets.symmetric(
+                        vertical: 10,
+                        horizontal: 15,
+                      ),
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.green,
+                          width: 2,
+                        ),
+                      ),
+                      padding: EdgeInsets.all(5),
+                      child: Text(
+                        "\$ ${trans.amount}",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: Colors.purple,
+                        ),
+                      ),
+                    ),
+                    Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            trans.title,
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text(
+                              DateFormat.yMMMd().format(trans.date),
+                              style: TextStyle(
+                                color: Colors.grey,
+                              )),
+                        ]),
+                  ],
+                ),
+              );
+            }).toList(),
+          ),
+        ],
       ),
     );
   }
